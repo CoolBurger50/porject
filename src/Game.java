b31d1ebda5d73978fae2736bd4166efa40a9c0db
2 <nothing@gmail.com>
2026-01-16 10:48:21 -0500

    67

--- src/Game.java
+++ src/Game.java
@@ -6,8 +6,13 @@ public class Game {
     private int totalSerpent = 3;
     private int totalPanther = 6;
     private int totalAmphithere = 8;
-    private int round = 0;
     private boolean tutorial = true;
+    private boolean fight = true;
+    private boolean condition = true;
+    private boolean potion = true;
+    private boolean village = true;
+
+
     private boolean quit = true;
     private Scanner scanner;
 
@@ -24,14 +29,51 @@ public class Game {
         System.out.println("If you defeat the basilisk the other monster may become passive again");
         checkHero();
         System.out.println("--------------------------------------------");
-        while (isWin()==false || quit==false) {
-            return;
+        while (totalBasilisk!=0 || quit==false) {
+            playRound();
         }
+        System.out.println("You did it and saved the Ashenshire Village");
+        System.out.println("Thanks, " + name);
+        System.out.println("This will be the end of this story and may you have a time.");
     }
 
     public void playRound() {
-        System.out.println("You enter the forest and see: ");
-        System.out.println("You have encountered a "  );
+        Scanner scanner = new Scanner(System.in);
+        if (tutorial) {
+            doTutorial();
+        }
+        System.out.println("What would you want to do?");
+        System.out.println("1. Attack monster");
+        System.out.println("2. Check condition");
+        System.out.println("3. Use potions");
+        System.out.println("4. Return to village");
+        System.out.println("5. Run away and leave");
+        System.out.println("Enter option: ");
+        int option = scanner.nextInt();
+        System.out.println("--------------------------------------------");
+        if (option==1) {
+
+        } else if (option==2) {
+
+        } else if (option==3) {
+
+        } else if (option==4) {
+
+        } else if (option==5) {
+
+        } else {
+            System.out.print("Bad input would you like the tutorial again? (y/n)");
+            String response = scanner.nextLine();
+            if (response.equals("y")) {
+                doTutorial();
+                quit=false;
+            } else if (response.equals("n")){
+                System.out.println("OK! Dont make the same mistake");
+            } else {
+                System.out.println("Input \"y\" or \"n\"");
+            }
+        }
+        System.out.println("--------------------------------------------");
     }
 
 
@@ -43,11 +85,40 @@ public class Game {
     }
 
     public void encouter() {
-        System.out.println("");
+        System.out.println("Here is the amount of monsters left in the forest");
+        checkRadar();
+        System.out.println("--------------------------------------------");
+        System.out.println("1. Amphithere");
+        System.out.println("2. Panther");
+        System.out.println("3. Serpent");
+        System.out.println("4. Basilisk");
+        System.out.print("Which monster do you want to fight: ");
+        int option = scanner.nextInt();
+        System.out.println("--------------------------------------------");
+        if (option==1) {
+
+        } else if (option==2) {
+
+        } else if (option==3) {
+
+        } else if (option==4) {
+
+        } else if (option==5) {
+
+        } else {
+        }
     }
 
-    public boolean isWin() {
-        return totalBasilisk==0;
+    public void doTutorial() {
+        System.out.println("Option 1: Attack a monster nearby");
+        System.out.println("Option 2: See you health and energy levels");
+        System.out.println("Option 3: Use potions you got in the village");
+        System.out.println("Option 4: You return to village to buy potions or rest");
+        System.out.println("Option 5: You run away like a coward");
+        System.out.println();
+        System.out.print("Press any key to continue ");
+        scanner.nextLine();
+        System.out.println("--------------------------------------------");
     }
 
     public void checkHero() {
